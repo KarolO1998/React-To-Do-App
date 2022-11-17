06b5dc144445d815055991fd5959fcbcaa65df3c
Karol <freespirits1998@gmail.com>
2022-11-16 21:58:03 -0500

    installed bootstrap

--- react-to-do-app/src/App.js
+++ react-to-do-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
